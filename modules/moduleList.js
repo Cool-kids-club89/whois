643f33ab41282866dbe81ea607aaeb023e45d73b
Barney (Kyle) <130295277+JamesHickers@users.noreply.github.com>
2026-01-04 09:01:39 -0500

    Create moduleList.js

---
+++ modules/moduleList.js
@@ -0,0 +1,17 @@
+{
+  "files": [
+    "user.js",
+    "github.js",
+    "display.js",
+    "aliases.js",
+    "fingerprint.js",
+    "display.js",
+    "graph.js",
+    "github.js",
+    "music.js",
+    "clustering.js",
+    "bio.js",
+    "org.js",
+    "display.js"
+  ]
+}
